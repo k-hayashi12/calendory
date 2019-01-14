43bd28fbd3b6b9724e96ba4c77e2444133cbaad0
k-hayashi12 <katsu.woods12@gmail.com>
2019-01-14 11:14:18 +0000

    [finish]イベント名編集

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,9 +16,13 @@ class EventsController < ApplicationController
 	end
 
 	def edit
+		@event = Event.find(params[:id])
 	end
 
 	def update
+		@event = Event.find(params[:id])
+		@event.update(event_params)
+		redirect_to event_path(@event.id)
 	end
 
 	def destroy
